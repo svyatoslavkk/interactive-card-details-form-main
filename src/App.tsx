902c3f45c62cbf55f84bdeb9951a87b39176c7cd
svyatoslavkk <sv.krivonos1@gmail.com>
2023-10-10 23:50:25 +0300

    mobile first done

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,31 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import bgMainMobile from './bg-main-mobile.png';
+import bgCartFront from './bg-card-front.png';
+import bgCartBack from './bg-card-back.png';
+import cardLogo from './card-logo.svg';
 import { CardForm } from './Components/CardForm';
 
 function App() {
 
   return (
     <div className='container'>
-      <img src={bgMainMobile} alt="Background Image" />
+      <img src={bgMainMobile} className='bg-main-mobile' alt="Background Image" />
+      <div className='bg-cart-back'>
+        <div className='relative-block'>
+          <img src={bgCartBack} alt='Card Back' />
+          <p className='cvc-on-img'>000</p>
+        </div>
+      </div>
+      <div className='bg-cart-front'>
+        <div className='relative-block'>
+          <img src={bgCartFront} alt="Card Front" />
+          <img src={cardLogo} className='card-logo' alt="Card Logo" />
+          <p className='number-on-img'>0000 0000 0000 0000</p>
+          <p className='cardholder-on-img'>Jane Appleseed</p>
+          <p className='date-on-img'>00/00</p>
+        </div>
+      </div>
       <CardForm />
     </div>
   );
