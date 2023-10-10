c07bf48fa133436814da480b37d72df1abcde0e4
svyatoslavkk <sv.krivonos1@gmail.com>
2023-10-10 23:01:56 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import bgMainMobile from './bg-main-mobile.png';
+import { CardForm } from './Components/CardForm';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <img src={bgMainMobile} alt="Background Image" />
+      <CardForm />
     </div>
   );
 }
---
+++ src/Components/CardForm.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+interface CardFormProps {
+    onSubmit: (formData: CardFormData) => void;
+}
+
+interface CardFormData {
+    cardHolderName: string;
+    cardNumber: string;
+    expiryMonth: string;
+    expiryYear: string;
+    cvv: string;
+}
+
+export const CardForm = () => {
+    const [formData, setFormData] = useState<CardFormData>({
+      cardHolderName: '',
+      cardNumber: '',
+      expiryMonth: '',
+      expiryYear: '',
+      cvv: '',
+    });
+
+    return (
+        <div>
+            <form className='form-block'>
+                <div className='cardholder-name-row'>
+                    <label>
+                        <h4 className='input-title'>Cardholder Name</h4>
+                        <input
+                            type="text"
+                            name="cardHolderName"
+                            placeholder='e.g. Jane Appleseed'
+                        />
+                    </label>
+                </div>
+                <div className='card-number-row'>
+                    <label>
+                        <h4 className='input-title'>Card Number</h4>
+                        <input
+                            type="text"
+                            name="cardNumber"
+                            placeholder='e.g. 1234 5678 9123 0000'
+                        />
+                    </label>
+                </div>
+                <div className='mm-yy-cvv-row'>
+                    <div className='expiry-month-year'>
+                        <label>
+                            <h4 className='input-title'>Exp. date (mm/yy)</h4>
+                            <div style={{display: 'flex'}}>
+                                    <input
+                                        type="text"
+                                        name="expiryMonth"
+                                        className='mm-input'
+                                        placeholder='MM'
+                                    />
+                                    <input
+                                        type="text"
+                                        name="expiryYear"
+                                        className='yy-input'
+                                        placeholder='YY'
+                                    />
+                            </div>
+                        </label>
+                    </div>
+                    <div className='cvc'>
+                        <label>
+                            <h4 className='input-title'>CVC</h4>
+                            <input
+                                type="text"
+                                name="cvv"
+                                placeholder='e.g. 123'
+                            />
+                        </label>
+                    </div>
+                </div>
+                <button type="submit" className='confirm-button'>Confirm</button>
+            </form>
+        </div>
+    )
+}
